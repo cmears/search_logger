db8d8c02a11eb84aa4b8b6ff69360c5996807c1d
Chris Mears <chris@cmears.id.au>
2015-11-10 13:49:10 +1100

    Experiment with nanomsg

--- search_logger.cpp
+++ search_logger.cpp
@@ -8,26 +8,41 @@
 #include <windows.h>
 #endif
 
-#include "zmq.hpp"
+// #include "zmq.hpp"
 #include "message.pb.hh"
+
+#include <nanomsg/nn.h>
+#include <nanomsg/tcp.h>
+#include <nanomsg/pipeline.h>
 
 int main() {
 
   using std::setw;
 
-	zmq::context_t context(1);
-	zmq::socket_t receiver (context, ZMQ_PULL);
-	receiver.bind("tcp://*:6565");
+  int nanosocket = nn_socket(AF_SP, NN_PULL);
+  
+	// zmq::context_t context(1);
+	// zmq::socket_t receiver (context, ZMQ_PULL);
+	// receiver.bind("tcp://*:6565");
+
+  nn_bind(nanosocket, "tcp://*:6565");
+  
   std::cout << "listening to port: 6565\n";
 
 	while (true) {
 	
-    zmq::message_t msg;
+    // zmq::message_t msg;
   
-		receiver.recv(&msg);
+    //     	receiver.recv(&msg);
+
+            void *buf = NULL;
+            int msg_length = nn_recv(nanosocket, &buf, NN_MSG, 0);
+            
+
+                usleep(10000);
 
     message::Node node;
-    node.ParseFromArray(msg.data(), msg.size());
+    node.ParseFromArray(buf, msg_length);
 
     if (node.type() == message::Node::NODE) {
       std::cout << std::left
@@ -54,6 +69,8 @@ int main() {
         std::cout << node.solution();
         std::cout << "------------------\n";
       }
+
+      nn_freemsg(buf);
 
       continue;
     }
